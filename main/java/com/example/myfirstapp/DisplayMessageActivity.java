0f3a4ef46f09dc5d3f14c54f74fa348b01080ebb
spadpaul <82052896+spadpaul@users.noreply.github.com>
2021-09-20 20:10:45 -0400

    App 1 Files
    All of the files for my first project

---
+++ main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ComponentActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        //Get Intent that started the string and recieve the String
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Get layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
